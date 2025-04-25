273a2704e21e7f6e3a526f51da1a83c587f799c5
zenon199 <abhishekyadav276208@gmail>
2025-04-25 21:44:49 +0530

    backend structure done

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const app = express();
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Port is running on ${process.env.PORT}`);
+})
